8ea30b39d0ecad44b512c108bffcb5d08fbb4ff2
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-10-16 15:49:38 -0400

    beforerender

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ team, teamMembers ] = useState({});
+
+	const addNewNote = (form) => {
+		teamMembers([ ...team, form ]);
+	};
+	return (
+		<div className='App'>
+			<Form addNewNote={addNewNote} />
+		</div>
+	);
 }
 
 export default App;
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Form = (props) => {
+	const [ form, setForm ] = useState({
+		name  : '',
+		email : '',
+		role  : '',
+	});
+	const ChangeHandler = (event) => {
+		setForm({ ...form, [event.target.name]: event.target.value });
+		console.log(event.target.value);
+	};
+	const submitForm = (event) => {
+		event.preventDefault();
+		props.addNewNote(form);
+		setForm({ name: '', email: '', role: '' });
+	};
+	return (
+		<form onSubmit={submitForm}>
+			<label htmlFor='name'>Name</label>
+			<input name='name' id='name' type='name' placeholder='name' onChange={ChangeHandler} value={form.name} />
+			<label htmlFor='email'>Email</label>
+			<input
+				name='email'
+				id='email'
+				type='email'
+				placeholder='Type your email here'
+				onChange={ChangeHandler}
+				value={form.email}
+			/>
+			<label htmlFor='role'>Role</label>
+			<textarea name='role' id='role' placeholder='role' onChange={ChangeHandler} value={form.role} />
+		</form>
+	);
+};
+export default Form;
